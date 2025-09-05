c7195160244267a1e42b414375b6660d849c5fd0
prabhueswar91 <prabhueswar91@gmail.com>
2025-09-05 12:27:15 +0530

    issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,14 +31,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-         <Script
-          src="https://telegram.org/js/telegram-web-app.js"
-          strategy="beforeInteractive"
-        />
-         <Script src="https://cdn.jsdelivr.net/npm/eruda" strategy="afterInteractive" />
-      <Script id="eruda-init" strategy="afterInteractive">
-        {`eruda.init();`}
-      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
