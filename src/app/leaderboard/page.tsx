90966adff20ede6072e1bed95502c45fb6499845
prabhueswar91 <prabhueswar91@gmail.com>
2026-01-09 16:04:50 +0530

    issues

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -61,6 +61,7 @@ export default function Leaderboard() {
 
     fetchUserPoints();
     fetchLeaderboard(activeTab,"");
+    setsValue("")
   }, [userId, activeTab]);
 
   const fetchUserPoints = async () => {
