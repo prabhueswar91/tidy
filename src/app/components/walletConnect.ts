8ca20d6be604fb5f92b540eb60e1dbef5262968b
prabhueswar91 <prabhueswar91@gmail.com>
2026-02-28 13:05:49 +0530

    issues

--- src/app/components/walletConnect.ts
+++ src/app/components/walletConnect.ts
@@ -3,32 +3,40 @@
 import { createAppKit } from '@reown/appkit/core'
 import { mainnet } from '@reown/appkit/networks'
 
-let modal: any = null
+let appKit: any = null
 
 export function getAppKit() {
-  if (modal) return modal
-
-  modal = createAppKit({
-    projectId: 'b01f86bb575d8820ed3e4337491b9685',
-    networks: [mainnet],
-    manualWCControl: true
-  })
-
-  return modal
+  if (!appKit) {
+    appKit = createAppKit({
+      projectId: 'b01f86bb575d8820ed3e4337491b9685',
+      networks: [mainnet],
+      manualWCControl: true
+    })
+  }
+  return appKit
 }
 
 export async function connectWallet1() {
   const modal = getAppKit()
-
+  alert('welcome')
+  // IMPORTANT: explicitly open modal
   modal.open()
 
-  const session = await modal.connect()
+  try {
+    const session = await modal.connect()
 
-  modal.close()
+    modal.close()
 
-  const accounts = session.namespaces.eip155.accounts
+    const accounts = session.namespaces.eip155.accounts
 
-  const address = accounts[0].split(':')[2]
+    const address = accounts[0].split(':')[2]
 
-  return address
+    return address
+
+  } catch (err) {
+    modal.close()
+    alert('errr')
+    alert(err)
+    throw err
+  }
 }
